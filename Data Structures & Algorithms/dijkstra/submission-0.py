37f6233eea3157c1efe2bba8c86d716b7afc5684
Peter Araujo <95590068+peteraraujo@users.noreply.github.com>
2026-04-26 17:48:32 -0400

    Add: dijkstra - submission-0

---
+++ Data Structures & Algorithms/dijkstra/submission-0.py
@@ -0,0 +1,34 @@
+from heapq import heappush, heappop
+from collections import defaultdict
+class Solution:
+    def shortestPath(self, n: int, edges: List[List[int]], src: int) -> Dict[int, int]:
+        
+        adj = defaultdict(list)
+
+        for f, t, w in edges:
+            adj[f].append((t, w))
+
+
+        mh = [(0, src)]
+        visited = set()
+
+        res = {vtx : -1 for vtx in range(n)}
+
+        while mh:
+            dist, node = heappop(mh)
+            if node in visited:
+                continue
+            
+            if res[node] == -1:
+                res[node] = dist
+            
+            for nei, w in adj[node]:
+                heappush(mh, (dist + w, nei))
+            
+            visited.add(node)
+        
+        return res
+
+            
+
+
