54eb56dc5b8cca76a9cea9062595b27f002a5207
Peter Araujo <95590068+peteraraujo@users.noreply.github.com>
2026-04-27 21:14:45 -0400

    Add: min-cost-to-connect-points - submission-2

---
+++ Data Structures & Algorithms/min-cost-to-connect-points/submission-2.py
@@ -0,0 +1,26 @@
+from collections import defaultdict
+from heapq import heappush, heappop
+
+class Solution:
+    def minCostConnectPoints(self, points: List[List[int]]) -> int:
+
+        minh = [(0, tuple(points[0]))] # (w, (x, y))
+        res = 0
+        visited = set() # (x, y)
+
+        while len(visited) != len(points):
+            w, coord = heappop(minh)
+
+            if coord in visited:
+                continue
+            
+            visited.add(coord)
+            res += w
+
+            xi, yi = coord
+            
+            for xj, yj in points:
+                if (xj, yj) not in visited:
+                    heappush(minh, (abs(xi - xj) + abs(yi - yj), (xj, yj)))
+        
+        return res
