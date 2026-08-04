e836c818ad2248b84829e90d01c6a3fe6adb78eb
Peter Araujo <95590068+peteraraujo@users.noreply.github.com>
2026-08-04 09:33:53 -0400

    Add: count-subsequences - submission-5

---
+++ Data Structures & Algorithms/count-subsequences/submission-5.py
@@ -0,0 +1,23 @@
+class Solution:
+    def numDistinct(self, s: str, t: str) -> int:
+        ssize = len(s)
+        tsize = len(t)
+
+        prevRow = [1] * (ssize + 1)
+        row = [0] * (ssize + 1)
+
+        for ti in range(tsize):
+            for si in range(ssize):
+                srowi = si + 1
+            
+                tchar, schar = t[ti], s[si]
+
+                take = prevRow[srowi - 1] if tchar == schar else 0
+                skip = row[srowi - 1]
+
+                row[srowi] = take + skip
+            
+            prevRow, row = row, prevRow
+            row[0] = 0
+        
+        return prevRow[-1]
