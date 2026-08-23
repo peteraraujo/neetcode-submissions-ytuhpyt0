934d821fe96302da84dd579ad94e0dd8b2d24a1c
Peter Araujo <95590068+peteraraujo@users.noreply.github.com>
2026-08-23 15:20:19 -0400

    Add: spiral-matrix - submission-8

---
+++ Data Structures & Algorithms/spiral-matrix/submission-8.py
@@ -0,0 +1,49 @@
+class Solution:
+    def spiralOrder(self, matrix: List[List[int]]) -> List[int]:
+        sizey, sizex = len(matrix), len(matrix[0])
+
+        dirs = ((0, 1), (1, 0), (0, -1), (-1, 0))
+
+        hcount = sizex
+        vcount = sizey - 1
+        
+
+        i = 0
+
+        y, x = 0, -1
+
+        res = []
+
+        while len(res) != len(matrix):
+            
+            # Horizontal
+            yd, xd = dirs[i]
+
+            for _ in range(hcount):
+                y += yd
+                x += xd
+                res.append(matrix[y][x])
+
+            i = (i + 1) % len(dirs)
+            hcount -= 1
+
+            if vcount == 0:
+                break
+
+
+            # Vertical
+            yd, xd = dirs[i]
+
+            for _ in range(vcount): 
+                y += yd
+                x += xd
+                res.append(matrix[y][x])
+
+            i = (i + 1) % len(dirs)
+            vcount -= 1
+
+            if hcount == 0:
+                break
+        
+
+        return res
