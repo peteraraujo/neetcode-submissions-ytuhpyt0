0b449824cdb08806110adb25da5c3b4442b0b45d
Peter Araujo <95590068+peteraraujo@users.noreply.github.com>
2026-08-27 20:39:34 -0400

    Add: matchsticks-to-square - submission-7

---
+++ Data Structures & Algorithms/matchsticks-to-square/submission-7.py
@@ -0,0 +1,39 @@
+from heapq import heapify, heappop, heappush
+
+class Solution:
+    def makesquare(self, matchsticks: List[int]) -> bool:
+        size = len(matchsticks)
+        total = sum(matchsticks)
+        maxMatchAllowed = total / 4
+        maxMatch = max(matchsticks)
+
+        if size < 4 or total % 4 != 0 or maxMatch > maxMatchAllowed:
+            return False
+        
+        buckets = [0, 0, 0, 0]
+
+        def dfs(i=0):
+
+            if i == size:
+                n = buckets[0]
+
+                for bi in range(1, 4):
+                    if buckets[bi] != n:
+                        return False
+                
+                return True
+            
+            for bi in range(4):
+                buckets[bi] += matchsticks[i]
+                if buckets[bi] > maxMatchAllowed:
+                    buckets[bi] -= matchsticks[i]
+                    continue
+                if dfs(i + 1):
+                    return True
+                buckets[bi] -= matchsticks[i]
+            
+            return False
+        
+        return dfs()
+
+            
